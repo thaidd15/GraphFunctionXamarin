637ae138c5158af82e890ef024d7db4583c0628c
thaidd15 <chooinlee15@gmail.com>
2018-10-25 22:58:27 +0700

    Changed formula integral

--- GraphFunction/GraphFunction/Model/Integral.cs
+++ GraphFunction/GraphFunction/Model/Integral.cs
@@ -92,10 +92,9 @@ namespace GraphFunction.Model
             {
                 return Double.NaN;
             }
-
-
+            
             x = a;
-            S = (exp.Evaluate_Postfix(a) + exp.Evaluate_Postfix(b));
+            S = (exp.Evaluate_Postfix(a) + exp.Evaluate_Postfix(b)) / 2;
             h = (b - a) / n;
             for (int i = 1; i <= n; i++)
             {
